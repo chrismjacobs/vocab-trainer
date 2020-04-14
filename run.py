0f16398691eb8cee97bbd7ab8827e9648faca216
ChrisEng0212 <cjx02121981@gmail.com>
2020-04-14 20:21:06 +0800

    socket io

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager, current_user 
+from flask_socketio import SocketIO
 from flask_cors import CORS
 from random import *
 import os
@@ -11,6 +15,19 @@ app = Flask(__name__,
         template_folder = "dist",
         )
 
+db = SQLAlchemy(app)
+bcrypt = Bcrypt()
+login = LoginManager(app)
+#login.login_view = 'login' # if user isn't logged in it will redirect to login page
+#login.login_message_category = 'info'
+
+socketio = SocketIO(app, manage_session=False)
+
+#@login.user_loader
+#def load_user(id):
+    #return User.query.get(int(id))
+
+  
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ## see documentation
@@ -38,5 +55,6 @@ def catch_all(path):
 
 
 if __name__ == '__main__': 
+    #socketio.run(app)
     app.run()
         
