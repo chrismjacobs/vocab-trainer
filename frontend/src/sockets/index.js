9bb5b1739b3c43f6ecd0762950c774c11493ca25
ChrisEng0212 <cjx02121981@gmail.com>
2020-08-27 01:07:10 +0800

    host socket

--- frontend/src/sockets/index.js
+++ frontend/src/sockets/index.js
@@ -8,7 +8,7 @@ export function openSocket () {
   if (host.includes('127') || host.includes('local')) {
     host = 'http://127.0.0.1:5000/'
   } else {
-    host = 'https://vc-bootcamp.herokuapp.com/'
+    host = 'https://vocab-lms.herokuapp.com/'
   }
   const socket = io.connect(host)
   let userProfile = store.state.userProfile
