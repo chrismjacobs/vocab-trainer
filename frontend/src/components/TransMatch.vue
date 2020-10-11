621c081c0312ac83f8b757a1a1c416ee9319395e
ChrisEng0212 <cjx02121981@gmail.com>
2020-10-11 23:03:12 +0800

    move progress bar

--- frontend/src/components/TransMatch.vue
+++ frontend/src/components/TransMatch.vue
@@ -7,14 +7,21 @@
             <h2 class="text-cream" > Match </h2>
           </b-col>
           <b-col cols="3" align="right">
-            <button @click="leave()" class="buttonDiv bg-warn text-cream"> Exit <b-icon-backspace-reverse-fill class="text-cream ml-3" style="float:right"  font-scale="1.5"></b-icon-backspace-reverse-fill> </button>
+            <button @click="leave()" class="buttonDiv bg-warn text-cream">Exit<b-icon-backspace-reverse-fill class="text-cream ml-3" style="float:right"  font-scale="1.5"></b-icon-backspace-reverse-fill> </button>
           </b-col>
         </b-row>
       </div>
 
-     <div class="bg-second p-3" v-if="showProgress">
-         <div class="mt-2 bg-third p-2" style="height:100px">
+     <div class="bg-second" v-if="showProgress">
+       <div class="bg-third" >
+         <b-progress  style="height:20px" :max="1"  class="" show-value>
+                <b-progress-bar :value="progressValues.p1" variant="p1"></b-progress-bar>
+                <b-progress-bar :value="progressValues.warn" variant="warn-light"></b-progress-bar>
+                <b-progress-bar :value="progressValues.p2" variant="p2"></b-progress-bar>
+        </b-progress>
+       </div>
 
+        <div class="bg-third p-3 " style="height:100px">
              <b-row no-gutters>
                <b-col align="right" class="mr-3">
                  <b-avatar :src="s3 + p1.toString() + '.jpg'"  size="65px" :badge="nameCut(p1name)" badge-offset="-0.6em" badge-variant="p1"></b-avatar>
@@ -31,21 +38,12 @@
                   <b-avatar v-if="ready.includes('p2')" icon="person-check" variant="safe"></b-avatar>
                </b-col>
              </b-row>
-
          </div>
-         <div>
-            <b-progress  style="height:30px" :max="1"  class="mt-2" show-value>
-                <b-progress-bar :value="progressValues.p1" variant="p1"></b-progress-bar>
-                <b-progress-bar :value="progressValues.warn" variant="warn-light"></b-progress-bar>
-                <b-progress-bar :value="progressValues.p2" variant="p2"></b-progress-bar>
-              </b-progress>
 
-            <br>
           <div style="height:30px">
             <b-progress v-if="time" :value="time" :max="timeReset" animated variant="warn"></b-progress>
           </div>
 
-          </div>
       </div>
 
     <ToolbarMatch :toolbarShow='showTest' :p1="p1" :p2="p2" :p1name="p1name" :p2name="p2name" :socket="socket" :player="player" :waiting="waiting" :showAnswers='showAnswers' :testType="testType" v-on:waitUpdate="waiting=1"></ToolbarMatch>
@@ -181,12 +179,12 @@ export default {
       let btnID = question + choice
       // show if player was correct of not
       let result = (choice === correct)
-      console.log('RESULT', result)
+      // console.log('RESULT', result)
 
       this.socket.emit('answer', {room: this.p1, name: question, answer: answer, btnID: btnID, player: this.player, state: result})
     },
     recordDisable: function () {
-      console.log('DISABLE RESULT')
+      // console.log('DISABLE RESULT')
 
       this.socket.emit('answer', {room: this.p1, name: null, answer: null, btnID: null, player: this.player, state: false})
     },
@@ -220,7 +218,7 @@ export default {
         button.classList.add(btnClass)
         button.disabled = true
         let buttons = document.getElementsByName(name)
-        console.log(buttons)
+        // console.log(buttons)
         if (player === this.player) {
           for (let i = 0; i < buttons.length; i++) {
             buttons[i].disabled = true
@@ -239,7 +237,7 @@ export default {
           buttons[i].disabled = true
         }
       }
-      console.log(state, this.answered)
+      // console.log(state, this.answered)
       if (state || this.answered === 1) {
         this.answered = 1
         this.nextQuestion(name, answer, player, state)
@@ -248,7 +246,7 @@ export default {
         this.answered = 1
         this.setCountdown()
       } else {
-        console.log('LOGIC ERROR')
+        // console.log('LOGIC ERROR')
       }
     },
     nextQuestion: function (name, answer, player, state) {
@@ -263,7 +261,7 @@ export default {
       }, 2000)
     },
     enterResult: function (question, answer, player, state) {
-      console.log(state)
+      // console.log(state)
 
       let _rowVariant = 'warn'
       let score = 0
@@ -280,15 +278,15 @@ export default {
       }
 
       this.progressValues[_rowVariant] += 1
-      console.log(_rowVariant, this.progressValues)
+      // console.log(_rowVariant, this.progressValues)
       this.answerData.push(entry)
     },
     filterToggle: function () {
       if (this.filter + 1 < this.testItems.length) {
-        console.log(this.filter, this.testItems.length)
+        // console.log(this.filter, this.testItems.length)
         this.filter += 1
       } else {
-        console.log('filterMax')
+        // console.log('filterMax')
         // go back to false
         this.filter = null
         this.showTest = false
@@ -316,7 +314,7 @@ export default {
       this.showAnswers = true
     },
     playAudio: function (arg) {
-      console.log('PLAY', arg)
+      // console.log('PLAY', arg)
       let player = document.getElementById('audio')
       player.src = arg
       player.play()
@@ -334,7 +332,7 @@ export default {
     },
     hover: function () {
       if (this.hover === true) {
-        console.log('hover_active')
+        // console.log('hover_active')
         let sound = this.testItems[this.filter]
         this.playAudio(sound.sdQue)
       }
@@ -344,9 +342,6 @@ export default {
     isAuthenticated () {
       return this.$store.getters.isAuthenticated
     }
-  },
-  beforeDestroy () {
-    console.log('beforeDestroyTransMatch')
   },
   mounted () {
     let _this = this
