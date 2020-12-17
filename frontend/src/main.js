c70001636ca03374bb6d5ea1cb125ccfb521d87c
ChrisEng0212 <cjx02121981@gmail.com>
2020-12-18 07:43:40 +0800

    global get clas function

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,7 @@ import store from './store/store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'vue2-animate/dist/vue2-animate.min.css'
+// import 'vue2-animate/dist/vue2-animate.min.css'
 
 Vue.config.productionTip = false
 
@@ -16,6 +16,24 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 /* eslint-disable no-new */
+
+Vue.mixin({
+  methods: {
+    getClass: function (bgc, txc, str) {
+      let scheme = store.state.scheme
+      let bg = ' '
+      let text = ' '
+      if (bgc !== null) {
+        bg = 'bg-' + scheme[bgc] + ' '
+      }
+      if (txc !== null) {
+        text = 'text-' + scheme[txc] + ' '
+      }
+      let classString = bg + text + str
+      return classString
+    }
+  }
+})
 
 new Vue({
   el: '#app',
