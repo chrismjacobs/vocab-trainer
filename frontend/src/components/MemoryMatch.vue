7e72642c77cfea6cf8a8406067bfee9e14bd7d3f
ChrisEng0212 <cjx02121981@gmail.com>
2021-01-03 02:01:17 +0800

    new activity

---
+++ frontend/src/components/MemoryMatch.vue
@@ -0,0 +1,611 @@
+<template>
+  <div class="MemoryMatch">
+    <audio id="audio"></audio>
+
+      <div class="bg-grape p-2 head">
+        <b-row align-h="end">
+          <b-col  class="d-none d-lg-inline">
+          </b-col>
+          <b-col align="center">
+            <h2 class="text-cream"> Memory </h2>
+          </b-col >
+          <b-col class="d-none d-lg-inline" align="right">
+            <button @click="leave()" class="buttonDiv bg-cream text-alert mt-1 mr-3" style="height:40px; width:100px"><span style="font-size:16pt" class="mr-2 mb-1">Exit</span><b-icon-backspace-reverse-fill  font-scale="1.5"></b-icon-backspace-reverse-fill> </button>
+          </b-col>
+        </b-row>
+      </div>
+
+     <div class="bg-second">
+
+        <div class="bg-third p-3 " style="height:100px">
+             <b-row no-gutters>
+               <b-col align="right" class="mr-3">
+                 <b-avatar :src="s3 + p1.toString() + '.jpg'"  size="65px" :badge="nameCut(p1name)" badge-offset="-0.6em" badge-variant="p1"></b-avatar>
+               </b-col>
+               <b-col align="left">
+                 <b-badge class="bg-smoke text-prime badge-lg mb-2" style="font-size:20px;width:40px">{{p1score}}</b-badge> <br>
+                 <b-avatar v-if="ready.includes('p1')" icon="person-check" variant="safe"></b-avatar>
+               </b-col>
+               <b-col align="right" class="mr-3">
+                  <b-avatar :src="s3 + p2.toString() + '.jpg'" size="65px" :badge="nameCut(p2name)" badge-offset="-0.6em" badge-variant="p2"></b-avatar>
+               </b-col>
+               <b-col align="left">
+                  <b-badge class="bg-smoke text-p2 badge-lg mb-2" style="font-size:20px;width:40px">{{p2score}}</b-badge> <br>
+                  <b-avatar v-if="ready.includes('p2')" icon="person-check" variant="safe"></b-avatar>
+               </b-col>
+             </b-row>
+         </div>
+
+          <div style="height:20px">
+            <b-progress v-if="showProgress" style="height:20px" :max="1"  class="" show-value>
+                    <b-progress-bar :value="progressValues.p1" variant="p1"></b-progress-bar>
+                    <b-progress-bar :value="progressValues.warn" variant="warn-light"></b-progress-bar>
+                    <b-progress-bar :value="progressValues.p2" variant="p2"></b-progress-bar>
+            </b-progress>
+          </div>
+      </div>
+
+    <ToolbarMatch
+    :toolbarShow='showTest'
+    :p1="p1" :p2="p2"
+    :p1name="p1name" :p2name="p2name"
+    :socket="socket" :player="player"
+    :waiting="waiting"
+    :showAnswers='showAnswers'
+    :testType="testType"
+    v-on:waitUpdate="waiting=1"
+    v-on:settings="updateSettings($event)"
+    ></ToolbarMatch>
+
+      <div align="center" class="bg-grey" v-if="showTest">
+          <div style="height:30px" class="d-block d-md-none">
+            <h4 v-if="foundCard && gameStyle[0] !== 'E'">{{foundCard.caption}}</h4>
+            <h4 v-else-if="foundCard">{{foundCard.English}}</h4>
+          </div>
+          <div style="height:30px" class="d-block d-md-none">
+            <h4 v-if="foundCard2 && gameStyle[0] !== 'E'">{{foundCard2.caption}}</h4>
+            <h4 v-else-if="foundCard2">{{foundCard2.English}}</h4>
+          </div>
+          <div v-for="(memcard, idx) in testItems" :key="idx" class="d-inline mb-3">
+            <button :class="getCardClass(memcard.caption)"  @click="showCard(memcard.caption, memcard.answer)" :id="memcard.caption" style="display:inline-block" :disabled="getDisabled(memcard.caption)">
+                <span class="textLine mt-3"> {{ memcard.code }} </span>
+                <br>
+                <span v-if="showStatus(memcard.caption) || gameStyle === 'SA1'" class="textLine mt-3">
+                  {{ memcard[cardText] }}
+                </span>
+            </button>
+          </div>
+      </div>
+
+      <div class="bg-smoke" v-if="showAnswers">
+      <div class="mt-2">
+        <b-table
+        striped hover
+        :items="answerData"
+        :fields="fields"
+        >
+        <template #head(question)="data">
+          <div align="right">
+            Question
+          </div>
+        </template>
+
+        <template v-slot:cell(question)="data">
+          <div style="float:left">
+           <template v-if="data.item.English in starGet">
+              <b-icon-star-fill variant="warn" @click="addStar(data.item.English, 0)"></b-icon-star-fill>
+            </template>
+            <template v-else>
+              <b-icon-star variant="grey" @click="addStar(data.item.English, 1)"></b-icon-star>
+            </template>
+          </div>
+          <div align="right">
+            <span :id="data.item.Question" @click="playAnswer(data.item.English)"> {{data.item.Question}}</span>
+          </div>
+        </template>
+
+        <template v-slot:cell(answer)="data">
+            <span :id="data.item.Answer" @click="playAnswer(data.item.English)"> {{data.item.Answer}}</span>
+        </template>
+
+        </b-table>
+      </div>
+    </div>
+
+    <b-modal align="center" ref="winner" hide-footer title="Game Over" hide-header-close no-close-on-esc no-close-on-backdrop>
+      <div class="d-block">
+        <h3> And the winner is... </h3>
+        <b-avatar :src="s3 + winner.toString() + '.jpg'"  size="100px" :badge="winName" badge-offset="-0.6em" :badge-variant="winner"></b-avatar>
+      </div>
+      <br>
+      <button :class="'buttonDiv mt-3 text-prime bg-' + player" style="width:60%"  @click="hideModal('winner')">Close</button>
+    </b-modal>
+
+   <b-modal align="center" ref="draw" hide-footer title="Problem Found" hide-header-close no-close-on-esc no-close-on-backdrop>
+      <div class="d-block">
+        <h3> It's a draw! </h3>
+      </div>
+      <button class="buttonDiv mt-3 bg-warn text-cream" style="width:60%"  @click="hideModal('draw')">Close</button>
+    </b-modal>
+
+  </div>
+</template>
+
+<script>
+import ToolbarMatch from './ToolbarMatch'
+
+export default {
+  name: 'MemoryMatch',
+  components: {
+    ToolbarMatch
+  },
+  props: {
+    testType: String,
+    p1: Number,
+    p1name: String,
+    p2: Number,
+    p2name: String,
+    player: String,
+    socket: Object,
+    s3: String,
+    gameOver: Boolean,
+    exit: Boolean
+  },
+  data () {
+    return {
+      waiting: 0,
+      pageHead: 'Memory Match',
+      toolbarShow: true,
+      showAnswers: false,
+      showTest: false,
+      showProgress: false,
+      timeReset: null,
+      hover: false,
+      ready: [],
+      answered: 0,
+      answerData: [],
+      filter: null,
+      testItems: [],
+      settings: {},
+      fields: ['Question', 'Answer'],
+      time: null,
+      clock: null,
+      clockBlock: false,
+      startTimer: null,
+      countDown: null,
+      progressValues: {
+        p1: 0,
+        p2: 0,
+        warn: 0
+      },
+      p1score: 0,
+      p2score: 0,
+      btnIDMarker: null,
+      playerDude: null,
+      winner: '',
+      winName: '',
+      audioMarker: null,
+      cardMatch: [],
+      disabledMarker: false,
+      foundCard: null,
+      foundCard2: null,
+      gameStyle: null
+    }
+  },
+  methods: {
+    addStar: function (word, set) {
+      this.$store.dispatch('newStar', {word: word, set: set})
+    },
+    showWinner: function () {
+      this.$refs['winner'].show()
+    },
+    showDraw: function () {
+      this.$refs['draw'].show()
+    },
+    hideModal: function (mode) {
+      if (mode === 'winner') {
+        this.$refs['winner'].hide()
+      } else if (mode === 'draw') {
+        this.$refs['draw'].hide()
+      }
+      this.progressValues.p1 = 0
+      this.progressValues.p2 = 0
+      this.progressValues.warn = 0
+      this.winner = ''
+      this.winName = ''
+    },
+    nameCut: function (name) {
+      let cut = name.split(' ')[0]
+      if (cut.length > 7) {
+        return cut[0]
+      } else {
+        return cut
+      }
+    },
+    start: function () {
+      console.log('START', this.player)
+      this.showAnswers = false
+      this.showProgress = true
+      this.filter = 0
+      this.answerData = []
+      this.showTest = true
+      // this.setCountdown()
+    },
+    readyCheck: function () {
+      console.log('length', this.ready, this.ready.length, this.player)
+      if (this.ready.length === 2) {
+        this.waiting = 2
+        if (this.player === 'p2') {
+          this.disabledMarker = true
+        }
+        let _this = this
+        _this.startTimer = setTimeout(function () {
+          _this.start()
+          // go to true
+          _this.waiting = 0
+          _this.ready = []
+          _this.startTimer = null
+        }, 3000)
+      }
+    },
+    getCardClass: function (arg) {
+      let found = this.testItems.find(element => element.caption === arg)
+
+      if (found.match) {
+        return 'basic text-prime bg-' + found.player
+      } else if (found.show) {
+        return 'basic text-prime bg-' + found.player + '-light'
+      } else if (this.disabledMarker) {
+        return 'basic text-cream bg-darkgrey'
+      } else {
+        return 'basic text-prime bg-warn-light'
+      }
+    },
+    getDisabled: function (arg) {
+      let found = this.testItems.find(element => element.caption === arg)
+
+      if (this.disabledMarker) {
+        return true
+      } else {
+        return found.disabled
+      }
+    },
+    showStatus: function (arg) {
+      let found = this.testItems.find(element => element.caption === arg)
+      return found.show
+    },
+    showCard: function (card, answer) {
+      console.log('cardMAtch', card, answer, this.cardMatch)
+      let match = false
+      let count = 1
+      // if card and answer are already marked
+      if (this.cardMatch.length === 2) {
+        if (this.gameStyle.includes('1')) {
+          this.disabledMarker = true
+        }
+        count = 2
+        if (this.cardMatch.includes(card)) {
+          match = true
+        }
+        this.cardMatch = []
+        this.socket.emit('answerMem', {room: this.p1 + '-' + this.p2, card: card, match: match, answer: answer, player: this.player, count: count})
+      } else if (this.cardMatch.length === 0) {
+        // add card and answer for checking
+        this.cardMatch.push(answer)
+        this.cardMatch.push(card)
+        this.socket.emit('answerMem', {room: this.p1 + '-' + this.p2, card: card, match: match, answer: answer, player: this.player, count: count})
+      } else {
+        this.disableAll()
+        this.cardMatch = []
+      }
+    },
+    enterResult: function (question, answer, clicker, state) {
+      console.log('ENTER', this.player)
+
+      let _rowVariant = clicker
+      let score = 0
+      let english = question
+
+      if (this.player === clicker) {
+        score = 1
+      }
+
+      let entry = {
+        Question: question,
+        Answer: answer,
+        _rowVariant: _rowVariant,
+        Score: score,
+        English: english
+      }
+
+      this.progressValues[_rowVariant] += 1
+      // console.log(_rowVariant, this.progressValues)
+      this.answerData.push(entry)
+
+      if (this.progressValues.p1 + this.progressValues.p2 === this.testItems.length / 2) {
+        this.checkAnswers()
+      }
+    },
+    checkAnswers: function () {
+      console.log('CHECK', this.player)
+
+      let p1C = this.progressValues.p1
+      let p2C = this.progressValues.p2
+
+      this.p1score += this.progressValues.p1
+      this.p2score += this.progressValues.p2
+
+      if (p1C > p2C) {
+        this.winner = 'p1'
+        this.winName = this.p1name
+        this.showWinner()
+      } else if (p2C > p1C) {
+        this.winner = 'p2'
+        this.winName = this.p2name
+        this.showWinner()
+      } else {
+        this.showDraw()
+      }
+
+      let winnerStatus
+      if (p1C === p2C) {
+        winnerStatus = 0
+      } else if (p1C > p2C && this.player === 'p1') {
+        winnerStatus = 1
+      } else if (p2C > p1C && this.player === 'p2') {
+        winnerStatus = 1
+      } else {
+        winnerStatus = -1
+      }
+
+      this.$store.dispatch('updateMatch', { mode: 'matchTrans', winnerStatus: winnerStatus })
+      this.$store.dispatch('updateRecord', { mode: 'matchTrans', answerData: this.answerData, settingsData: this.settings })
+      this.showAnswers = true
+      this.showTest = false
+      this.disabledMarker = false
+    },
+    updateSettings: function (data) {
+      console.log('update active', data)
+      this.gameStyle = data.gameStyle
+    },
+    playAudio: function (arg) {
+      console.log('PLAY', arg, this.player)
+      let player = document.getElementById('audio')
+      player.src = arg
+      player.play()
+    },
+    playAnswer: function (arg) {
+      console.log(arg)
+      var found = this.dictGet.find(element => element.English === arg)
+
+      console.log('PLAY', found)
+      let audioLink = found.mp3en
+
+      if (this.audioMarker) {
+        let icon = document.getElementById(this.audioMarker)
+        icon.setAttribute('class', '')
+      }
+
+      this.audioMarker = arg
+      let icon = document.getElementById(arg)
+
+      icon.setAttribute('class', 'text-warn')
+
+      let player = document.getElementById('audio')
+      player.src = audioLink
+      player.play()
+      player.onended = function () {
+        icon.setAttribute('class', '')
+      }
+    },
+    leave: function () {
+      this.$emit('leave', {})
+      // this.socket.emit('exitMatch', { player: this.player, p1: this.p1, p2: this.p2 })
+    }
+  },
+  watch: {
+    filter: function () {
+      let sound = this.testItems[this.filter]
+      if (sound && this.sound !== 'sdOff') {
+        this.playAudio(sound.sdQue)
+      }
+    },
+    gameOver: function () {
+      this.showTest = false
+      this.showAnswers = true
+      this.waiting = 3
+      clearInterval(this.clock)
+    },
+    hover: function () {
+      if (this.hover === true) {
+        // console.log('hover_active')
+        let sound = this.testItems[this.filter]
+        this.playAudio(sound.sdQue)
+      }
+    },
+    exit: function () {
+      this.leave()
+    }
+  },
+  computed: {
+    starGet () {
+      return this.$store.getters.starGet
+    },
+    dictGet () {
+      return this.$store.getters.makeList
+    },
+    cardText () {
+      if (this.gameStyle === 'SA1') {
+        return 'caption'
+      } else if (this.gameStyle[0] === 'E') {
+        return 'English'
+      } else if (this.gameStyle[0] === 'T') {
+        return 'caption'
+      }
+    }
+  },
+  created () {
+    let options = {
+      p1: 'p1',
+      p2: 'p2'
+    }
+    this.playerDude = options[this.player]
+  },
+  beforeDestroy () {
+    console.log('LEAVE')
+    this.answered = 0
+    this.filter = null
+    this.showTest = false
+
+    if (this.clock) {
+      clearInterval(this.clock)
+      this.clock = null
+    }
+    if (this.startTimer) {
+      clearTimeout(this.startTimer)
+      this.startTimer = null
+    }
+    if (this.countDown) {
+      clearTimeout(this.countDown)
+      this.countDown = null
+    }
+  },
+  mounted () {
+    let _this = this
+    _this.socket.on('go', function (data) {
+      console.log('roomReady', data, data.testItems.length)
+      _this.room = data.room
+      if (data.testItems.length > 0) {
+        console.log(data.testItems)
+        _this.testItems = data.testItems
+        _this.timeReset = data.timeReset * 1000
+      }
+      if (!_this.ready.includes(data.player)) {
+        _this.ready.push(data.player)
+        console.log(data.player, _this.ready)
+      }
+      _this.readyCheck()
+    })
+    _this.socket.on('answerCard', function (data) {
+      console.log('answerCard', data)
+      let found = _this.testItems.find(element => element.caption === data.card)
+      let answer = _this.testItems.find(element => element.caption === data.answer)
+      console.log(found, answer)
+      if (data.count === 1) {
+        found.show = true
+        found.player = data.player
+        found.disabled = true
+        _this.foundCard = found
+      } else if (data.count === 2) {
+        found.show = true
+        found.player = data.player
+        found.disabled = true
+        _this.foundCard2 = found
+        let xThis = _this
+        if (!data.match) {
+          console.log(22222)
+          _this.disabledMarker = true
+          setTimeout(function () {
+            found.show = false
+            xThis.foundCard.show = false
+            found.player = null
+            xThis.foundCard.player = null
+            found.disabled = false
+            xThis.foundCard.disabled = false
+            xThis.foundCard = null
+            xThis.foundCard2 = null
+            if (data.player !== xThis.player) {
+              xThis.disabledMarker = false
+            }
+          }, 2000)
+        } else { // if match
+          _this.disabledMarker = true
+          found.disabled = true
+          _this.foundCard.disabled = true
+          found.match = true
+          _this.foundCard.match = true
+          _this.playAudio(found.sound)
+          _this.enterResult(found.English, found.Chinese, data.player, true)
+          let xThis = _this
+          setTimeout(function () {
+            xThis.foundCard = null
+            xThis.foundCard2 = null
+            if (data.player !== xThis.player && xThis.gameStyle.includes('1')) {
+              xThis.disabledMarker = false
+            }
+            if (data.player === xThis.player && xThis.gameStyle.includes('2')) {
+              xThis.disabledMarker = false
+            }
+          }, 2000)
+        }
+      }
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.mistake {
+  border:4px solid #d35944;
+  color: #205372 !important
+}
+
+.correct {
+  border:4px solid #48c490;
+  color: #205372 !important
+}
+
+.basic {
+  border:0px solid #CCC;
+  outline:none;
+  border-radius: 5px;
+  box-shadow: 0 0 5px -1px rgba(0, 0, 0, 0.2);
+  cursor:pointer;
+  margin: 1px;
+  text-align: center;
+  width:24%;
+  height:100px;
+  align-items: center;
+}
+
+@media screen and (max-width:650px) {
+  .basicc {
+    width:130px;
+    height:60px;
+    line-height: 60px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px
+  }
+}
+
+.textLine {
+  display: inline-block;
+  vertical-align: middle;
+  word-break: break-all;
+  font-size: 24px;
+}
+
+.basic:active {
+    box-shadow: 0 0 5px -1px rgba(0,0,0,0.6);
+    border:0px solid #CCC;
+    outline:none;
+}
+
+.basic:hover {
+    box-shadow: 0 0 5px -1px rgba(0,0,0,0.6);
+    border:0px solid #CCC;
+    outline:none;
+}
+
+@media screen and (max-width:650px) {
+  .textLine {
+  font-size: 16px;
+  word-break: break-all;
+  }
+}
+
+</style>
