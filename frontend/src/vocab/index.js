b0c1c677d6664372a9ca42a4a99548f6e70c8bba
ChrisEng0212 <cjx02121981@gmail.com>
2022-01-12 16:05:21 +0800

    export json fetch

---
+++ frontend/src/vocab/index.js
@@ -0,0 +1,47 @@
+import tourism1 from '../assets/json/tourism1.json'
+import tourism from '../assets/json/tourism.json'
+import digital1 from '../assets/json/digital1.json'
+import culinary1 from '../assets/json/culinary1.json'
+import culinary2 from '../assets/json/culinary2.json'
+import generalY from '../assets/json/generalY.json'
+import generalW from '../assets/json/generalW.json'
+import generalD from '../assets/json/generalD.json'
+import generalV from '../assets/json/generalV.json'
+import generalT from '../assets/json/generalT.json'
+import generalG from '../assets/json/generalG.json'
+import generalJW1 from '../assets/json/generalJW1.json'
+import generalBell from '../assets/json/generalBell.json'
+import generalAlice from '../assets/json/generalAlice.json'
+import jHotel from '../assets/json/japanHotel.json'
+import LNC1 from '../assets/json/LNCV.json'
+import presentation from '../assets/json/presentation.json'
+import food from '../assets/json/food.json'
+import helpj from '../assets/json/help.json'
+// import Bella from '../assets/json/BellaRedo.json'
+// import test from '../assets/json/vqc2.json'
+// import cul from '../assets/json/cul.json'
+export function dictionaries () {
+  const dictionaries = {
+    'tourism1': tourism1,
+    'tourism': tourism,
+    'digital1': digital1,
+    'culinary1': culinary1,
+    'culinary2': culinary2,
+    'generalY': generalY,
+    'generalW': generalW,
+    'generalD': generalD,
+    'generalV': generalV,
+    'generalT': generalT,
+    'generalG': generalG,
+    'generalBell': generalBell,
+    'generalJW1': generalJW1,
+    'generalAlice': generalAlice,
+    'LNC1': LNC1,
+    'presentation': presentation,
+    'high': null,
+    'food': food,
+    'JapanHotel': jHotel,
+    'helpj': helpj
+  }
+  return dictionaries
+}
