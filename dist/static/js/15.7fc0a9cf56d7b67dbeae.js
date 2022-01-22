816938b4015e9cf5544ce28f7754cec06137f549
ChrisEng0212 <cjx02121981@gmail.com>
2022-01-22 18:56:02 +0800

    new build

--- dist/static/js/15.7fc0a9cf56d7b67dbeae.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([15],{ivEo:function(e,t){},lO7g:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("b-container")],1)},staticRenderFns:[]};var a=n("VU/8")({data:function(){return{tableItems:null}},methods:{},created:function(){}},r,!1,function(e){n("ivEo")},"data-v-0fbefd02",null);t.default=a.exports}});
-//# sourceMappingURL=15.7fc0a9cf56d7b67dbeae.js.map
