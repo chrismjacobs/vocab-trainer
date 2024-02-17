5904f9606eac1b23b80b61513661402c39dc3e1c
chrismjacobs <jacobsen_christian@yahoo.co.uk>
2024-02-17 22:43:47 +0800

    host page

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -6,7 +6,8 @@ let host = window.location.hostname
 if (host.includes('127') || host.includes('local')) {
   host = 'http://127.0.0.1:5000'
 } else {
-  host = 'https://vocab-app-uryj.onrender.com/'
+  page = window.location.href.split('/')[2] + '/'
+  host = 'https://' + page
 }
 
 export function register (userData) {
